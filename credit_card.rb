e655f9ac103a72e2ccad6a51604d05d1e0ed087f
charleneshawn <19.chsw@gmail.com>
2026-03-12 17:49:00 +0800

    Finish

--- credit_card.rb
+++ credit_card.rb
@@ -1,22 +1,32 @@
 # frozen_string_literal: true
 
-require_relative './luhn_validator'
+require_relative 'luhn_validator'
 require 'json'
 
+# CreditCard class
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
+  include LuhnValidator
 
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
-  def to_json
+  def to_json(*_args)
     {
       # TODO: setup the hash with all instance vairables to serialize into json
+      number: @number,
+      expiration_date: @expiration_date,
+      owner: @owner,
+      credit_network: @credit_network
     }.to_json
   end
 
@@ -28,6 +38,13 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    data = JSON.parse(card_s)
+    number = data['number']
+    expiration_date = data['expiration_date']
+    owner = data['owner']
+    credit_network = ['credit_network']
+
+    CreditCard.new(number, expiration_date, owner, credit_network)
   end
 
   # return a cryptographically secure hash
@@ -35,5 +52,6 @@ class CreditCard
     # TODO: implement this method
     #   - Use sha256 to create a cryptographically secure hash.
     #   - Credit cards with identical information should produce the same hash
+    Digest::SHA256.hexdigest(to_json)
   end
 end
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,11 +1,20 @@
+# frozen_string_literal: true
+
+# Luhn Validator
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
   def validate_checksum
-    nums_a = number.to_s.chars.map(&:to_i)
-
-    # TODO: use the integers in nums_a to validate its last check digit
+    nums_a = number.to_s.chars.map(&:to_i).reverse
+    sum = nums_a.each_with_index.sum do |digit, i|
+      if i.odd?
+        digit *= 2
+        digit -= 9 if digit > 9
+      end
+      digit
+    end
+    (sum % 10).zero?
   end
 end
